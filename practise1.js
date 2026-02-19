029760f4a16f10103cb01b26a838880c9c8f801e
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-02-19 22:35:05 +0530

    initial

--- practise1.js
+++ practise1.js
@@ -1,15 +1,73 @@
-const input = document.getElementById("taskInput");
-const button = document.getElementById("addBtn");
-const list = document.getElementById("taskList");
+ 
+// todo-app 1st code js
 
-button.addEventListener("click", function () {
-  const taskText = input.value;
+// const input = document.getElementById("taskInput");
+// const button = document.getElementById("addBtn");
+// const list = document.getElementById("taskList");
 
-  if (taskText === "") return;
+// button.addEventListener("click", function () {
+//   const taskText = input.value;
 
-  const li = document.createElement("li");
-  li.textContent = taskText;
+//   if (taskText === "") return;
 
-  list.appendChild(li);
-  input.value = "";
-});
+//   const li = document.createElement("li");
+//   li.textContent = taskText;
+
+//   list.appendChild(li);
+//   input.value = "";
+// });
+
+// console.log("Hello JavaScript");
+
+// let city = "Bhopal";
+// console.log(city);
+
+// let name = "Anshu";   
+// let age = 20;         
+// let isStudent = true;
+// let x = null;         
+// let y;      
+
+// console.log(typeof name);
+// console.log(typeof age);
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+
+
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+
+
+// loops
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+
+// while loop
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+
+
+
+
