207466aee5afd1f71f34714f6b0c76bb1ce4510f
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-03-10 01:21:15 +0530

    initial

---
+++ dom3.js
@@ -0,0 +1,29 @@
+// 10 march 2026 gpt Js
+// querySelector()
+// let heading = document.querySelector(".title");
+
+// console.log(heading);
+
+
+
+
+// ID Selection
+// let h = document.querySelector("#heading");
+
+// h.textContent = "Learning JS";
+
+
+
+
+// Tag selection
+let p = document.querySelector("p");
+
+console.log(p);
+
+
+
+
+// querySelectorAll()
+let paragraphs = document.querySelectorAll("p");
+
+console.log(paragraphs);
